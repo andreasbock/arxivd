2c623fabf03498da9d6fc4b2eefacb67ebacb455
MisanthropicBit <alexander.asp.bock@gmail.com>
2016-12-25 22:45:11 +0100

    Added basic command line functionality

--- src/Application/CommandLineParser.hs
+++ src/Application/CommandLineParser.hs
@@ -1,1 +1,35 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Application.CommandLineParser where
+
+import Control.Monad (when)
+import System.Console.Docopt
+import System.Environment (getArgs)
+import System.Exit
+
+programName :: String
+programName = "arxivd"
+
+version :: String
+version = "0.1.0"
+
+usageText :: Docopt
+usageText = [docoptFile|USAGE.docopt|]
+
+-- |Process the command line using a given docopt description and a list of strings
+processCommandLine :: Docopt -> [String] -> IO ()
+processCommandLine usage cmdline = do
+    args <- parseArgsOrExit usage cmdline
+    let getArgOrExit = getArgOrExitWith usage
+
+    when (args `isPresent` longOption "help") $ exitWithUsage usage
+    when (args `isPresent` longOption "version") $ putStrLn (programName ++ " v" ++ version)
+        >> exitSuccess
+    when (args `isPresent` longOption "subject") $ do
+        name <- args `getArgOrExit` longOption "subject"
+        putStrLn ("Subject: " ++ name)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    processCommandLine usageText args
