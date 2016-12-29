843e7bec790680fff1b2b714b08486e196c6138e
MisanthropicBit <alexander.asp.bock@gmail.com>
2016-12-29 21:13:55 +0100

    Added Author and Entry data types

--- src/Parser/Types.hs
+++ src/Parser/Types.hs
@@ -1,1 +1,20 @@
 module Parser.Types where
+
+import Data.DateTime
+
+data Author = Author {
+    name :: String, -- Or first and last name
+    affiliation :: String
+}
+
+data Entry = Entry {
+    id         :: String,
+    updated    :: DateTime,
+    published  :: DateTime,
+    title      :: String,
+    summary    :: String,
+    authors    :: [Author],
+    doi        :: String,
+    comment    :: String,
+    journalRef :: String
+}
