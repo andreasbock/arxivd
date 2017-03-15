baedcdc90e65b805484b660bab65113416a07f83
MisanthropicBit <alexander.asp.bock@gmail.com>
2017-03-15 15:31:24 -0500

    Added processing and date functions

--- src/Application/CommandLineParser.hs
+++ src/Application/CommandLineParser.hs
@@ -3,7 +3,11 @@
 module Application.CommandLineParser where
 
 import Control.Monad (when)
+import Data.Time.Clock (UTCTime, utctDay)
+import Data.Time.Calendar (toGregorian)
 import Data.List (intercalate)
+import Data.List.Split (splitOn)
+import Data.Time (getCurrentTime)
 import System.Console.Docopt
 import System.Environment (getArgs)
 import System.Exit
@@ -17,6 +21,38 @@ version = "0.1.0"
 
 usageText :: Docopt
 usageText = [docoptFile|USAGE.docopt|]
+
+data Date = Date {
+    day :: Int,
+    month :: Int,
+    year :: Integer
+} deriving Show
+
+data Options = Options {
+    subjects   :: [String],
+    published  :: Date,
+    title      :: String,
+    ignoreCase :: Bool
+} deriving Show
+
+currentDate :: IO (Integer, Int, Int)
+currentDate = fmap (toGregorian . utctDay) getCurrentTime
+
+processArgWithDefault :: Arguments -> Option -> a -> (String -> a) -> a
+processArgWithDefault args opt def processor =
+    case getArg args opt of
+        Just v -> processor v
+        _      -> def
+
+processSubjects :: String -> [String]
+processSubjects "" = []
+processSubjects s  = splitOn "," s
+
+processPublished :: String -> IO Date
+processPublished _ = currentDate >>= Date
+
+{-processDate :: String -> Maybe DateTime-}
+{-processDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"-}
 
 -- |Process the command line using a given docopt description and a list of strings
 processCommandLine :: Docopt -> [String] -> IO ()
